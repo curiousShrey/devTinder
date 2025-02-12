798f233278c1266251537f36bc0a00853656fb07
Shreyansh Joshi <shreyanshjoshi@Shreyanshs-MacBook-Pro.local>
2025-02-12 23:43:14 +0530

    Added dummy GET route

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,6 +7,10 @@ app.use("/test",(req,res)=>{
     res.send("Hello from the server test route!");
 })
 
+app.get("/users",(req,res)=> {
+    res.send("Got all the response of users.")
+})
+
 app.listen(3000, ()=> {
     console.log("Server is successfully listening on port 3000");
 }); //pass on the port where you want to start the server.
