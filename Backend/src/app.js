65d8a2e8aec81cc6a85e9a921d61b82e40e187c0
Shreyansh Joshi <shreyanshjoshi@Shreyanshs-MacBook-Pro.local>
2025-02-23 00:54:07 +0530

    Signup data validation + hashing password

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const connectDB = require('./config/database');
 const User = require('./models/user');
+const bcrypt = require("bcrypt");
+const { validateSignUpData } = require("./utils/validation");
 
 const app = express(); // creating a new app using Express (a new project basically)
 
@@ -23,19 +25,46 @@ connectDB().then(()=>{
 
 
 app.post('/signup', async (req,res)=>{
+    try {
+      // Validation of data
+      validateSignUpData(req);
+  
+      const { firstName, lastName, emailId, password } = req.body;
+  
+      // Encrypt the password
+      const passwordHash = await bcrypt.hash(password, 10);
+      console.log(passwordHash);
+  
+      //   Creating a new instance of the User model
+      const user = new User({
+        firstName,
+        lastName,
+        emailId,
+        password: passwordHash,
+      });
+  
+      const savedUser = await user.save();
+      const token = await savedUser.getJWT();
+  
+      res.cookie("token", token, {
+        expires: new Date(Date.now() + 8 * 3600000),
+      });
+  
+      res.json({ message: "User Added successfully!", data: savedUser });
+    } catch (err) {
+      res.status(400).send("ERROR : " + err.message);
+    }
+  });
 
-    const userObj = req.body;
+app.post('/login', async (req,res) =>{
+    const loginData = req.body;
+    console.log(loginData.emailId);
 
-    const user = new User(userObj);
-
-    await user.save().then(()=>{
-
-        res.send("User added successfully!")
-        
+    await User.find({emailId: loginData.emailId}).then((user)=>{
+        console.log(user);
+        res.send("User logged in succesfully.")
     }).catch(err => {
-        console.log(err);
-        res.status(400).send("User not added.")
-    });
+        throw new Error('User not found.')
+    })
 
-   
 })
--- Backend/src/models/user.js
+++ Backend/src/models/user.js
@@ -1,13 +1,95 @@
 const mongoose = require('mongoose');
+const validator = require("validator");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
-const userSchema = new mongoose.Schema({
-    firstName: { type: String },
-    lastName: { type: String },
-    emailId: { type: String },
-    password: { type: String },
-    age: {type: Number},
-    gender: { type: String }
-})
+const userSchema = new mongoose.Schema(
+    {
+      firstName: {
+        type: String,
+        required: true,
+        minLength: 4,
+        maxLength: 50,
+      },
+      lastName: {
+        type: String,
+        required: true,
+      },
+      emailId: {
+        type: String,
+        lowercase: true,
+        required: true,
+        unique: true,
+        trim: true,
+        validate(value) {
+          if (!validator.isEmail(value)) {
+            throw new Error("Invalid email address: " + value);
+          }
+        },
+      },
+      password: {
+        type: String,
+        required: true,
+        validate(value) {
+          if (!validator.isStrongPassword(value)) {
+            throw new Error("Enter a Strong Password: " + value);
+          }
+        },
+      },
+      age: {
+        type: Number,
+        min: 18,
+      },
+      gender: {
+        type: String,
+        enum: {
+          values: ["male", "female", "other"],
+          message: `{VALUE} is not a valid gender type`,
+        },
+        // validate(value) {
+        //   if (!["male", "female", "others"].includes(value)) {
+        //     throw new Error("Gender data is not valid");
+        //   }
+        // },
+      },
+      isPremium: {
+        type: Boolean,
+        default: false,
+      },
+      membershipType: {
+        type: String,
+      },
+      photoUrl: {
+        type: String,
+        default: "https://static.vecteezy.com/system/resources/previews/009/292/244/non_2x/default-avatar-icon-of-social-media-user-vector.jpg",
+        validate(value) {
+          if (!validator.isURL(value)) {
+            throw new Error("Invalid Photo URL: " + value);
+          }
+        },
+      },
+      about: {
+        type: String,
+        default: "This is a default about of the user!",
+      },
+      skills: {
+        type: [String],
+      },
+    },
+    {
+      timestamps: true,
+    }
+  );
+
+userSchema.methods.getJWT = async function () {
+    const user = this;
+  
+    const token = await jwt.sign({ _id: user._id }, "DEV@Tinder$790", {
+      expiresIn: "7d",
+    });
+  
+    return token;
+  };
 
 
 module.exports = mongoose.model("User", userSchema);
